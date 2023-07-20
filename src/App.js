6f25b95b5c3224601d668159c8d5a47d38023b23
kharedevansh11 <devanshkhare11@gmail.com>
2023-07-20 23:00:19 +0530

    Implementing Api and adding banner

--- src/App.js
+++ src/App.js
@@ -1,32 +1,17 @@
 import React from 'react';
 import { Counter } from './features/counter/Counter';
 import Homescreen from './components/Homescreen';
+
 import './App.css';
 
 function App() {
   return (
     <div className="app">
       <Homescreen/>
-      <h1>Test</h1>
-      <h1>Test</h1>
-      <h1>Test</h1>
-      <h1>Test</h1>
-      <h1>Test</h1>
-      <h1>Test</h1>
-      <h1>Test</h1>
-      <h1>Test</h1>
-      <h1>Test</h1>
-      <h1>Test</h1>
-      <h1>Test</h1>
-      <h1>Test</h1>
-      <h1>Test</h1>
-      <h1>Test</h1>
-      <h1>Test</h1>
-      <h1>Test</h1>
-      <h1>Test</h1>
-      <h1>Test</h1>
-      <h1>Test</h1>
-      <h1>Test</h1>
+      
+      
+
+
     </div>
   );
 }
